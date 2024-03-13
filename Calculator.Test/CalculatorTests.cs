51f9841d7d6efe054526ccf3055da78a38a69168
AcciGen <denrov13@gmail.com>
2024-03-13 18:16:47 +0500

    tests added

---
+++ Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿global using Xunit;
+using FluentAssertions;
+
+namespace Calculator.Test
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            CalculatorBase calculator = new CalculatorBase();
+
+            //Act
+            var result = calculator.Add(2, 3);
+            int expectedResult = 5;
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(5, 3)]
+        [InlineData(7, 5)]
+        public void SubtractTest(int a, int b)
+        {
+            CalculatorBase calculator = new CalculatorBase();
+            
+            var result = calculator.Subtract(a, b);
+
+            result.Should().BePositive();
+        }
+
+        [Theory]
+        [InlineData(2, 3, 6)]
+        [InlineData(4, 2, 8)]
+        public void MultiplyTest(int a, int b, int expectedResult)
+        {
+            CalculatorBase calculator = new CalculatorBase();
+
+            var result = calculator.Multiply(a, b);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void DivideTest()
+        {
+            CalculatorBase calculator = new CalculatorBase();
+
+            var result = calculator.Divide(5, 1);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ Calculator/CalculatorBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class CalculatorBase
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
